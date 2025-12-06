04cbd5af333c95e7d3b45d0657df02895b7beb2d
SylaneCpn <sylane.campan@gmail.com>
2025-12-06 20:32:09 +0100

    Update advent_of_code_2025.dart

--- lib/advent_of_code_2025.dart
+++ lib/advent_of_code_2025.dart
@@ -2,3 +2,5 @@ export 'package:advent_of_code_2025/day1.dart';
 export 'package:advent_of_code_2025/day2.dart';
 export 'package:advent_of_code_2025/day3.dart';
 export 'package:advent_of_code_2025/day4.dart';
+export 'package:advent_of_code_2025/day5.dart';
+export 'package:advent_of_code_2025/day6.dart';
